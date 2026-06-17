e092be6bbae2f94e271b56ad06918fee1a3f1e0b
RyanS4 <ryan.snyder@nhrec.org>
2026-06-17 01:05:44 -0400

    feat: add navbar element spacing

--- green-day-website/src/components/NavBar.jsx
+++ green-day-website/src/components/NavBar.jsx
@@ -22,11 +22,11 @@ export default function NavBar() {
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                     Green Day
                 </Typography>
-                <Button variant="outlined" color="inherit" onClick={() => handleNavClick("/")}>Home</Button>
-                <Button variant="outlined" color="inherit" onClick={() => handleNavClick("/about")}>About</Button>
-                <Button variant="outlined" color="inherit" onClick={() => handleNavClick("/lawn-programs")}>Lawn Programs</Button>
-                <Button variant="outlined" color="inherit" onClick={() => handleNavClick("/services")}>Services</Button>
-                <Button variant="outlined" color="inherit" onClick={() => handleNavClick("/contact")}>Contact</Button>
+                <Button variant="outlined" color="inherit" sx={{ marginLeft: 3 }} onClick={() => handleNavClick("/")}>Home</Button>
+                <Button variant="outlined" color="inherit" sx={{ marginLeft: 3 }} onClick={() => handleNavClick("/about")}>About</Button>
+                <Button variant="outlined" color="inherit" sx={{ marginLeft: 3 }} onClick={() => handleNavClick("/lawn-programs")}>Lawn Programs</Button>
+                <Button variant="outlined" color="inherit" sx={{ marginLeft: 3 }} onClick={() => handleNavClick("/services")}>Services</Button>
+                <Button variant="outlined" color="inherit" sx={{ marginLeft: 3 }} onClick={() => handleNavClick("/contact")}>Contact</Button>
             </Toolbar>
         </AppBar>
     );
