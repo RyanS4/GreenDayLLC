470e21f143df9f082ea2846362e2ebcdd047189d
RyanS4 <ryan.snyder@nhrec.org>
2026-06-22 16:48:43 -0400

    style: card text center align

--- green-day-website/src/Pages/LawnPrograms.jsx
+++ green-day-website/src/Pages/LawnPrograms.jsx
@@ -14,20 +14,20 @@ function LawnPrograms() {
             <Box sx={{display: 'flex', width: '100%',}}>
                 <Box sx={{flex: 1, p: 2, display: 'flex', flexDirection: 'column'}}>
                     <Card sx={{ margin: 2, padding: 2, backgroundColor: '#E8F5E9', display: 'flex', flexDirection: 'column', height: '100%' }}>
-                        <Typography variant="h5" component="div" gutterBottom sx={{ fontWeight: 'bold', color : '#06402B' }}>
+                        <Typography variant="h5" component="div" gutterBottom sx={{ fontWeight: 'bold', color : '#06402B', justifyContent: 'center', display: 'flex' }}>
                             Step 1: Early Spring Application (Late February – March)
                         </Typography>
-                        <Typography variant="body1" component="div">
+                        <Typography variant="body1" component="div" align='center' sx={{justifyContent: 'center', display: 'flex'}}>
                             This is a liquid application that covers the entire lawn. It contains pre-emergent for the prevention of Crabgrass and other weedy-grasses, broadleaf herbicide to eliminate existing weeds, and a light rate of nitrogen to improve color.
                         </Typography>
                     </Card>
                 </Box>
                 <Box sx={{flex: 1, p: 2, display: 'flex', flexDirection: 'column'}}>
                     <Card sx={{ margin: 2, padding: 2, backgroundColor: '#E8F5E9', display: 'flex', flexDirection: 'column', height: '100%' }}>
-                        <Typography variant="h5" component="div" gutterBottom sx={{ fontWeight: 'bold', color : '#06402B' }}>
+                        <Typography variant="h5" component="div" gutterBottom sx={{ fontWeight: 'bold', color : '#06402B', justifyContent: 'center', display: 'flex' }}>
                             Step 2: Late Spring Application (April – Mid May)
                         </Typography>
-                        <Typography variant="body1" component="div">
+                        <Typography variant="body1" component="div" align='center' sx={{justifyContent: 'center', display: 'flex'}}>
                             This is a liquid application that covers the entire lawn. It contains pre-emergent for the prevention of Crabgrass and other weedy-grasses, broadleaf herbicide to eliminate existing and newly emerging summer weeds, and a very light rate of nitrogen to maintain color.
                         </Typography>
                     </Card>
@@ -36,20 +36,20 @@ function LawnPrograms() {
                 <Box sx={{ display: 'flex', width: '100%',}}>
                     <Box sx={{flex: 1, p: 2, display: 'flex', flexDirection: 'column'}}>
                          <Card sx={{ margin: 2, padding: 2, backgroundColor: '#E8F5E9', display: 'flex', flexDirection: 'column', height: '100%' }}>
-                        <Typography variant="h5" component="div" gutterBottom sx={{ fontWeight: 'bold', color : '#06402B' }}>
+                        <Typography variant="h5" component="div" gutterBottom sx={{ fontWeight: 'bold', color : '#06402B', justifyContent: 'center', display: 'flex' }}>
                             Step 3: Early Summer Application (Mid May – June)
                         </Typography>
-                        <Typography variant="body1" component="div">
+                        <Typography variant="body1" component="div" align='center' sx={{justifyContent: 'center', display: 'flex'}}    >
                             A summer-safe, iron, and potassium-rich fertilizer is used now. Summer weeds are treated at this time. Certain summertime weeds are very resilient and may not be completely removed by one treatment. Examples include Yellow Nutsedge, Dallisgrass, wild Violets, and Virginia Buttonweed.
                         </Typography>
                     </Card>
                     </Box>
                     <Box sx={{flex: 1, p: 2, display: 'flex', flexDirection: 'column'}}>
                     <Card sx={{ margin: 2, padding: 2, backgroundColor: '#E8F5E9', display: 'flex', flexDirection: 'column', height: '100%' }}>
-                        <Typography variant="h5" component="div" gutterBottom sx={{ fontWeight: 'bold', color : '#06402B' }}>
+                        <Typography variant="h5" component="div" gutterBottom sx={{ fontWeight: 'bold', color : '#06402B', justifyContent: 'center', display: 'flex' }}>
                             Step 4: Late Summer Application (July – August)
                         </Typography>
-                        <Typography variant="body1" component="div">
+                        <Typography variant="body1" component="div" align='center' sx={{justifyContent: 'center', display: 'flex'}}>
                             Based on your soil test results, we alternate years with either Organic Compost-type fertilizer or Pelletized Lime to improve soil composition and nutrient uptake. This will also be beneficial for any seeding done in the fall. Summer weeds are treated at this time.
                         </Typography>
                     </Card>
@@ -58,20 +58,20 @@ function LawnPrograms() {
             <Box sx={{ display: 'flex', width: '100%',}}>  
                 <Box sx={{flex: 1, p: 2, display: 'flex', flexDirection: 'column'}}>
                     <Card sx={{ margin: 2, padding: 2, backgroundColor: '#E8F5E9', display: 'flex', flexDirection: 'column', height: '100%' }}>
-                        <Typography variant="h5" component="div" gutterBottom sx={{ fontWeight: 'bold', color : '#06402B' }}>
+                        <Typography variant="h5" component="div" gutterBottom sx={{ fontWeight: 'bold', color : '#06402B', justifyContent: 'center', display: 'flex' }}>
                             Step 5: Early Fall Application (September – Mid October)
                         </Typography>
-                        <Typography variant="body1" component="div">
+                        <Typography variant="body1" component="div" align='center' sx={{justifyContent: 'center', display: 'flex'}}>
                             Proper rate of Special Blend granular fertilizer on lawn and spot weed control. *If you are also one of our Core Aeration and Seeding customers, your Early Fall application will be done on the same day as your Aeration service.
                         </Typography>
                     </Card>
                 </Box>
                 <Box sx={{ flex: 1, p: 2, display: 'flex', flexDirection: 'column'}}>  
                     <Card sx={{ margin: 2, padding: 2, backgroundColor: '#E8F5E9', display: 'flex', flexDirection: 'column', height: '100%' }}>
-                        <Typography variant="h5" component="div" gutterBottom sx={{ fontWeight: 'bold', color : '#06402B' }}>
+                        <Typography variant="h5" component="div" gutterBottom sx={{ fontWeight: 'bold', color : '#06402B', justifyContent: 'center', display: 'flex' }}>
                             Step 6: Late Fall Application (Mid October – Mid November)
                         </Typography>
-                        <Typography variant="body1" component="div">
+                        <Typography variant="body1" component="div" align='center' sx={{justifyContent: 'center', display: 'flex'}}>
                             Heavy rate of balanced fertilizer with macro-nutrients and spot weed control.
                         </Typography>
                     </Card>
@@ -80,10 +80,10 @@ function LawnPrograms() {
             <Box sx={{ display: 'flex', width: '100%', alighnSelf: 'left', p: 2}}>
                 <Box sx={{flex: 1, p: 2, justifyContent: 'center', display: 'flex'}}>
                     <Card sx={{ margin: 2, padding: 2, backgroundColor: '#E8F5E9' }}>
-                        <Typography variant="h5" component="div" gutterBottom sx={{ fontWeight: 'bold', color : '#06402B' }}>
+                        <Typography variant="h5" component="div" gutterBottom sx={{ fontWeight: 'bold', color : '#06402B', justifyContent: 'center', display: 'flex' }}>
                             Step 7: Winter Application (Mid November – December)
                         </Typography>
-                        <Typography variant="body1" component="div">
+                        <Typography variant="body1" component="div" align='center' sx={{justifyContent: 'center', display: 'flex'}}>
                             Heavy rate of balanced fertilizer to “winterize” the lawn and set up for quick recovery in the following spring. Spot weed control.
                         </Typography>
                     </Card>
@@ -93,7 +93,7 @@ function LawnPrograms() {
                 <Typography variant="h5" component="div" gutterBottom sx={{ fontWeight: 'bold', color : '#06402B', justifyContent: 'center', display: 'flex' }}>
                     Also Available
                 </Typography>
-                <Typography variant="body1" component="div" sx={{ justifyContent: 'center', display: 'flex', fontSize: '20px' }}> 
+                <Typography variant="body1" component="div" align='center' sx={{ justifyContent: 'center', display: 'flex', fontSize: '20px' }}> 
                     5 step weed & fertilization program for bermuda and zoysia lawns
                 </Typography>
             </Box>
--- green-day-website/src/Pages/Services.jsx
+++ green-day-website/src/Pages/Services.jsx
@@ -5,6 +5,10 @@ import NavBar  from "../components/NavBar";
 import InfoBlock from "../components/InfoBlock";
 import backflow from '../assets/backflow.png';
 import corearation from '../assets/coreaeration.jpg';
+import brownpatch from '../assets/brownpatch.jpg';
+import soiltesting from '../assets/soiltesting.jpg';
+import lawnrenovation from '../assets/lawnrenovation.jpg';
+import pestcontrol from '../assets/pestcontrol.jpg';
 
 function Services() {
 	return (
@@ -16,10 +20,10 @@ function Services() {
             <Box sx={{display: 'flex', width: '100%',}}>
                 <Box sx={{flex: 1, p: 2, display: 'flex', flexDirection: 'column'}}>
                     <Card sx={{ margin: 2, padding: 2, backgroundColor: '#E8F5E9', display: 'flex', flexDirection: 'column', height: '100%' }}>
-                        <Typography variant="h5" component="div" gutterBottom sx={{ fontWeight: 'bold', color : '#06402B' }}>
+                        <Typography variant="h5" component="div" gutterBottom sx={{ fontWeight: 'bold', color : '#06402B', justifyContent: 'center', display: 'flex' }}>
                             Core Aeration and Seeding (Late February – March)
                         </Typography>
-                        <Typography variant="body1" component="div">
+                        <Typography variant="body1" component="div" align="center" sx={{justifyContent: 'center', display: 'flex'}}>
                             Aeration can provide the necessary boost to a marginal lawn and a rejuvenation to others. As nature breaks up the cores, the soil will filter into the turf and support the microbial decomposition of the undisturbed thatch. Air and water entering the coring holes will also enhance the decomposition of the thatch while promoting deeper root growth.
                         </Typography>
                         <img src={corearation} alt="Core Aeration and Seeding" style={{ width: '50%', marginTop: '15px', borderRadius: '5px', display: 'block', marginLeft: 'auto', marginRight: 'auto' }} />
@@ -27,60 +31,64 @@ function Services() {
                 </Box>
                 <Box sx={{flex: 1, p: 2, display: 'flex', flexDirection: 'column'}}>
                     <Card sx={{ margin: 2, padding: 2, backgroundColor: '#E8F5E9', display: 'flex', flexDirection: 'column', height: '100%' }}>
-                        <Typography variant="h5" component="div" gutterBottom sx={{ fontWeight: 'bold', color : '#06402B' }}>
+                        <Typography variant="h5" component="div" gutterBottom sx={{ fontWeight: 'bold', color : '#06402B', justifyContent: 'center', display: 'flex' }}>
                            Turf Disease Control
                         </Typography>
-                        <Typography variant="body1" component="div">
+                        <Typography variant="body1" component="div" align="center" sx={{justifyContent: 'center', display: 'flex'}}>
                            The most common and damaging turf disease in our area is Brown Patch. The disease starts as water-soaked lesions on the leaf blade or sheath, quickly fading to tan. Severely affected patches of grass appear yellow and sunken, giving the turf a pockmarked look. High activity levels of Brown Patch are associated with periods of warm-to-hot muggy nights with light showers which occur after a period of drought. Fungicide treatments are available as needed.
                         </Typography>
+                        <img src={brownpatch} alt="Brown Patch" style={{ width: '50%', marginTop: '8%', borderRadius: '5px', display: 'block', marginLeft: 'auto', marginRight: 'auto' }} />
                     </Card>
                 </Box>
             </Box>
                 <Box sx={{ display: 'flex', width: '100%',}}>
                     <Box sx={{flex: 1, p: 2, display: 'flex', flexDirection: 'column'}}>
                          <Card sx={{ margin: 2, padding: 2, backgroundColor: '#E8F5E9', display: 'flex', flexDirection: 'column', height: '100%' }}>
-                        <Typography variant="h5" component="div" gutterBottom sx={{ fontWeight: 'bold', color : '#06402B' }}>
+                        <Typography variant="h5" component="div" gutterBottom sx={{ fontWeight: 'bold', color : '#06402B', justifyContent: 'center', display: 'flex' }}>
                            Lawn Renovations
                         </Typography>
-                        <Typography variant="body1" component="div">
+                        <Typography variant="body1" component="div" align="center" sx={{justifyContent: 'center', display: 'flex'}}>
                            When it is best to start over from scratch, a lawn renovation is recommended. We begin in late August – killing all undesired weeds and wild Bermuda. Two weeks later we return for a second spray to achieve a thorough kill. Once we are satisfied that we have eliminated all that we can, it’s time to core aerate, double-seed with sod-quality fescue, and apply starter fertilizer. All that is left to do is keep it watered.
                         </Typography>
+                        <img src={lawnrenovation} alt="Lawn Renovation" style={{ width: '50%', marginTop: '8%', borderRadius: '5px', display: 'block', marginLeft: 'auto', marginRight: 'auto' }} />
                     </Card>
                     </Box>
                     <Box sx={{flex: 1, p: 2, display: 'flex', flexDirection: 'column'}}>
                     <Card sx={{ margin: 2, padding: 2, backgroundColor: '#E8F5E9', display: 'flex', flexDirection: 'column', height: '100%' }}>
-                        <Typography variant="h5" component="div" gutterBottom sx={{ fontWeight: 'bold', color : '#06402B' }}>
+                        <Typography variant="h5" component="div" gutterBottom sx={{ fontWeight: 'bold', color : '#06402B', justifyContent: 'center', display: 'flex' }}>
 							Pest Control
                         </Typography>
-                        <Typography variant="body1" component="div">
+                        <Typography variant="body1" component="div" align="center" sx={{justifyContent: 'center', display: 'flex'}}>
                            Any lawn can have a wide variety of insect populations at any given time. Some insects, like white grubs or chinchbugs, can cause damage to yards. Others, such as fleas, ticks, and fire ants are bothersome to pets and people. Insects can live above the soil level in the plants or thatch layer or below the soil level in the roots. Insects can also attract other pests, such as moles. Contact us today, we can build a custom program to address your lawn care needs.
                         </Typography>
+                        <img src={pestcontrol} alt="Pest Control" style={{ width: '50%', marginTop: '8%', borderRadius: '5px', display: 'block', marginLeft: 'auto', marginRight: 'auto' }} />
                     </Card>
                 </Box>
             </Box>
             <Box sx={{ display: 'flex', width: '100%',}}>  
                 <Box sx={{flex: 1, p: 2, display: 'flex', flexDirection: 'column'}}>
                     <Card sx={{ margin: 2, padding: 2, backgroundColor: '#E8F5E9', display: 'flex', flexDirection: 'column', height: '100%' }}>
-                        <Typography variant="h5" component="div" gutterBottom sx={{ fontWeight: 'bold', color : '#06402B' }}>
+                        <Typography variant="h5" component="div" gutterBottom sx={{ fontWeight: 'bold', color : '#06402B', justifyContent: 'center', display: 'flex' }}>
                             Soil Testing
                         </Typography>
-                        <Typography variant="body1" component="div">
+                        <Typography variant="body1" component="div" align="center" sx={{justifyContent: 'center', display: 'flex'}}>
                            Soil testing is beneficial for the measurement of nutrients in the lawn, the pH of the soil, and the combination or ratio of phosphorus, potassium, and nitrogen. The pH should be determined as some grass types grow better in acidic soil whilst others require a less acidic environment.
 						</Typography>
+                        <img src={soiltesting} alt="Soil Testing" style={{ width: '50%', marginTop: '15px', borderRadius: '5px', display: 'block', marginLeft: 'auto', marginRight: 'auto' }} />
 					</Card>
                 </Box>
                 <Box sx={{ flex: 1, p: 2, display: 'flex', flexDirection: 'column'}}>  
                     <Card sx={{ margin: 2, padding: 2, backgroundColor: '#E8F5E9', display: 'flex', flexDirection: 'column', height: '100%' }}>
-                        <Typography variant="h5" component="div" gutterBottom sx={{ fontWeight: 'bold', color : '#06402B' }}>
+                        <Typography variant="h5" component="div" gutterBottom sx={{ fontWeight: 'bold', color : '#06402B', justifyContent: 'center', display: 'flex' }}>
                             <strong>NEW SERVICE: </strong> Backflow Repair and Testing
                         </Typography>
-                        <Typography variant="body1" component="div" sx={{marginBottom: 2}}>
+                        <Typography variant="body1" component="div" align="center" sx={{justifyContent: 'center', display: 'flex', marginBottom: 2}}>
 							The purpose of a backflow prevention assembly is to keep the outside water (which can contain contaminates such as fertilizer, pesticides, ect.) from back flowing and entering into your drinking water or the water provider's system.
                         </Typography>
-						<Typography variant="body1" component="div" sx={{marginBottom: 2}}>
+						<Typography variant="body1" component="div" align="center" sx={{justifyContent: 'center', display: 'flex', marginBottom: 2}}>
 							Annual testing of your backflow prevention assembly is required by the Virginia Department of Health and your local water provider.
 						</Typography>
-						<Typography variant="body1" component="div">
+						<Typography variant="body1" component="div" align="center" sx={{justifyContent: 'center', display: 'flex'}}>
 							<strong>All documentation forwarded to Local Water Authority</strong>
 						</Typography>
                             <img src={backflow} alt="Backflow Testing" style={{ width: '50%', marginTop: '15px', borderRadius: '5px', display: 'block', marginLeft: 'auto', marginRight: 'auto' }} />
