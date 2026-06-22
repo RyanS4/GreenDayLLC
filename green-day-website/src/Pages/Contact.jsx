5e5d89bb161f137ba52e7b14693c619593ae5e95
RyanS4 <ryan.snyder@nhrec.org>
2026-06-22 19:11:44 -0400

    feat: added green day logo image

--- green-day-website/src/Pages/Contact.jsx
+++ green-day-website/src/Pages/Contact.jsx
@@ -2,18 +2,26 @@ import { Typography, Box } from '@mui/material';
 import NavBar from '../components/NavBar';
 import PageHeader from '../components/PageHeader';
 import InfoBlock from '../components/InfoBlock';
+import greenday from '../assets/greenday.jpg';
 
 function Contact() {
     return (
         <Box sx={{ flexGrow: 1 }}>
             <NavBar />
             <PageHeader title="Contact Green Day Lawn Service" />
-            <Box sx={{ padding: 2, display: 'flex', flexDirection: 'column', gap: 2 }}>
-                <Typography variant="h4">Contact Information</Typography>
-                <Typography variant="h6" sx={{ fontStyle: 'italic' }}>Call us anytime</Typography>
-                <Typography><strong>Phone:</strong> (757) 365-4849</Typography>
-                <Typography><strong>Email:</strong> John@greendayllc.net</Typography>
-                <Typography><strong>Mailing Address:</strong> PO Box 219 Smithfield, VA. 23431</Typography>
+            {// leave less of a gap between the header and the other content below
+            }  
+            <Box sx={{ display: 'flex', flexDirection: { xs: 'column', md: 'row' }, width: '100%', alignItems: 'center', justifyContent: 'center' }}>
+                <Box sx={{ flex: 1, p: 2, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+                    <Typography variant="h4" sx={{ fontWeight: 'bold' }}>Contact Information</Typography>
+                    <Typography variant="h6" sx={{ fontStyle: 'italic' }}>Call us anytime</Typography>
+                    <Typography sx={{ fontSize: '22px' }}><strong>Phone:</strong> (757) 365-4849</Typography>
+                    <Typography sx={{ fontSize: '22px' }}><strong>Email:</strong> John@greendayllc.net</Typography>
+                    <Typography sx={{ fontSize: '22px' }}><strong>Mailing Address:</strong> PO Box 219 Smithfield, VA. 23431</Typography>
+                </Box>
+                 <Box sx={{ flex: 1, p: 2, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+                    <img src={greenday} alt="Green Day Lawn Service" style={{ width: '100%', height: 'auto', borderRadius: '8px' }} />
+                </Box>
             </Box>
         </Box>
     );
